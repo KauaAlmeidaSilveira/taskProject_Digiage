135618f423e92996525d2006c75f89d651b97f4f
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-09 23:46:02 -0300

    Task 1 completed and java version updated to 21

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -13,6 +13,28 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
-     
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a string para verificar se é um palindromo: ");
+        String resp = sc.nextLine();
+
+        System.out.println(isPalindromo(resp) ? "É um palindromo": "Não é um palindromo");
+
+        sc.close();
+    }
+
+    public static boolean isPalindromo(String palindromo) {
+        StringBuilder palindromoInvertido = new StringBuilder();
+
+        for (int i = palindromo.length() - 1; i >= 0; i--) {
+            // Adiciona cada caractere da string original, invertendo sua ordem
+            palindromoInvertido.append(palindromo.charAt(i));
+        }
+
+        // Retorna true se a string invertida for igual à string original, caso contrário, retorna false
+        return palindromoInvertido.toString().equals(palindromo);
+    }
  
 }
