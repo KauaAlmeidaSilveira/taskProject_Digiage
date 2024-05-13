3e4f6360d5cbf4b732e11c8f1195c6217232f9f7
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-13 09:24:30 -0300

    initialize task 5

---
+++ TASK5/projectTask5/src/main/java/com/digiage/projectTask5/ProjectTask5Application.java
@@ -0,0 +1,19 @@
+package com.digiage.projectTask5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Create an implementation of a Rest API .
+ * Expose an API. Feel free to explore possibilities/functionalities/capabilities following Rest standard.
+ * We suggest that your implementation have at least a CRUD scenario.
+ *
+ */
+@SpringBootApplication
+public class ProjectTask5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTask5Application.class, args);
+	}
+
+}
